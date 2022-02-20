2bbfdee49127882d82964220df01285d865f52a6
jimalaros <jimalaros25@gmail.com>
2022-02-20 15:19:51 -0500

    data github

--- src/routes/auth/github.js
+++ src/routes/auth/github.js
@@ -20,9 +20,9 @@ router.get(
   }),
   function (req, res) {
     console.log(`Peticion get /${strategy_name}/callback `);
-    const data = req.user;
-    console.log("Data:");
-    console.log(data);
+    const username = req.user.displayName;
+    console.log("Username:");
+    console.log(username);
     //const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
     //const url_front = process.env.URL_FRONT + `/?token=${token}`;
 
