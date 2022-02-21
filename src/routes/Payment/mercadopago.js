0f9536b60f31496567fb5967233037de5aed95d2
jimalaros <jimalaros25@gmail.com>
2022-02-20 21:50:51 -0500

    mercadopago

--- src/routes/Payment/mercadopago.js
+++ src/routes/Payment/mercadopago.js
@@ -36,9 +36,9 @@ const Pedidos = async (req, res) => {
         quantities.push(b);
         let c = precios[index];
         prices.push(c);
-        ordenes.push({ title: names[index], unit_price: prices[index], quantity: quantities[index] })
-        console.log(ordenes);
+        ordenes.push({ title: names[index], unit_price: prices[index], quantity: quantities[index] })W  
       }
+      console.log(ordenes);
     } else {
       console.log("No hay pedidos para mostrar");
     }
