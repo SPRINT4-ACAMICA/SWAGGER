790351430caab8c9a450b1cfb04253f9f6918860
jimalaros <jimalaros25@gmail.com>
2022-02-26 14:57:54 -0500

    Constante

--- src/routes/Payment/datos.js
+++ src/routes/Payment/datos.js
@@ -7,37 +7,34 @@ let prices = [];
 let ordenes = [];
 
 export const Pedidos = async (req, res) => {
-  try {
-    const pedidos = await Pedido.find();
-    if (pedidos) {
-      res.json(pedidos);
-      const info = pedidos[pedidos.length - 1].pedidos;
-      const nombres = info[info.length - 1].nombres;
-      const cantidades = info[info.length - 1].cantidades;
-      //console.log(nombres);
-      const vector = await Producto.find({ nombre: { $in: nombres } });
-      const precios = vector.map((price) => price.precio);
-      //console.log(prices);
-      //console.log(nombres);
-      //console.log(cantidades);
-      for (let index = 0; index < nombres.length; index++) {
-        let a = nombres[index];
-        names.push(a);
-        let b = cantidades[index];
-        quantities.push(b);
-        let c = precios[index];
-        prices.push(c);
-        ordenes.push({
-          title: names[index],
-          unit_price: prices[index],
-          quantity: quantities[index],
-        });
-      }
-      //console.log(ordenes);
-    } else {
-      console.log("No hay pedidos para mostrar");
+  const pedidos = await Pedido.find();
+  if (pedidos) {
+    res.json(pedidos);
+    const info = pedidos[pedidos.length - 1].pedidos;
+    const nombres = info[info.length - 1].nombres;
+    const cantidades = info[info.length - 1].cantidades;
+    //console.log(nombres);
+    const vector = await Producto.find({ nombre: { $in: nombres } });
+    const precios = vector.map((price) => price.precio);
+    //console.log(prices);
+    //console.log(nombres);
+    //console.log(cantidades);
+    for (let index = 0; index < nombres.length; index++) {
+      let a = nombres[index];
+      names.push(a);
+      let b = cantidades[index];
+      quantities.push(b);
+      let c = precios[index];
+      prices.push(c);
+      ordenes.push({
+        title: names[index],
+        unit_price: prices[index],
+        quantity: quantities[index],
+      });
     }
-  } catch (error) {
-    console.log(error);
+    return ordenes;
+    //console.log(ordenes);
+  } else {
+    console.log("No hay pedidos para mostrar");
   }
 };
