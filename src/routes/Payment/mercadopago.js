48661ddec7c26a78eb61a236eca3ee6971b6dc1e
jimalaros <jimalaros25@gmail.com>
2022-02-26 15:04:45 -0500

    Constante

--- src/routes/Payment/mercadopago.js
+++ src/routes/Payment/mercadopago.js
@@ -70,7 +70,7 @@ router.post("/pago", async function (req, res) {
 
   let items = [];
 
-  let vector = await Pedidos.pedidos;
+  let vector = await Pedidos.ordenes();
   console.log(vector);
 
   for (let index = 0; index < vector.length; index++) {
