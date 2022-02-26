30544b47720bff002971741a353c2c92db2155e6
jimalaros <jimalaros25@gmail.com>
2022-02-26 10:21:47 -0500

    ACCESS TOKEN FACEBOOK

--- src/routes/auth/facebook.js
+++ src/routes/auth/facebook.js
@@ -23,11 +23,12 @@ router.get(
     failureRedirect: "/failed",
   }),
   function (req, res) {
-    console.log(`Peticion get /${strategy_name}/callback `);
-    const username = req.user.name;
-    console.log(
+    //console.log(`Peticion get /${strategy_name}/callback `);
+    const data = req.user._json;
+    console.log(data);
+    /**console.log(
       `Nombre de usuario: ${username.givenName} ${username.familyName}`
-    );
+    );**/
     //const token = "hgjsd8fs6g7s7df67g6sdf43sdg2s3df5sg6s7df7";
     //const url_front = process.env.URL_FRONT + `/?token=${token}`;
 
