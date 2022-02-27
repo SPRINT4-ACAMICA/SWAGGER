8329298ffa54b40802b9ded3538624c5c51eafca
jimalaros <jimalaros25@gmail.com>
2022-02-26 21:50:31 -0500

    NO SCHEMA

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -4,12 +4,29 @@ import facebook from "./facebook.js";
 import linkedin from "./linkedin.js";
 import github from "./github.js";
 
-import  datosLogin from "../../middlewares/login.middleware.js";
-import  { inicioSesion } from "../../controllers/usuarios.controller.js";
+//import  datosLogin from "../../middlewares/login.middleware.js";
+//import  { inicioSesion } from "../../controllers/usuarios.controller.js";
 
 const router = Router();
 
-router.post("/Login", datosLogin, inicioSesion);
+//router.post("/Login", datosLogin, inicioSesion);
+
+router.post("/sign-up", function (req, res) {
+    console.log("New request POST to /sign-up");
+  
+    console.log(req.body);
+  
+    const token = "hfdasjfefee23423fewfrw24234";
+  
+    let data = {
+      success: true,
+      message: `User ${req.body.email} registered correctly`,
+      token: token,
+      data: req.body,
+    };
+  
+    res.json(data);
+  });  
 
 router.get("/failed", (req, res) => res.send("Hay un error en el login"));
 
