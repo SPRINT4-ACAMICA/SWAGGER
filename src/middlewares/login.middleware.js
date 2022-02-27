c0c1f46256c788d9153d6edbf72d9949bdd0b6fa
jimalaros <jimalaros25@gmail.com>
2022-02-26 21:28:53 -0500

    MIDDLEWARE

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -2,11 +2,11 @@ import { sessionSchema } from "../schemas/login.schema.js";
 
 const datosLogin = async (req, res, next) => {
   try {
-    await sessionSchema(req.body);
+    await sessionSchema.validateAsync(req.body);
     next();
   } catch (error) {
-    console.error(error);
-    res.status(404).json(error);
+    console.error(error.details[0].message);
+    res.status(404).json(error.details[0].message);
   }
 };
 
