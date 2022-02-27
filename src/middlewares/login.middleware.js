c76bc98aa2ce90fd431df6526343733795608a6c
jimalaros <jimalaros25@gmail.com>
2022-02-26 21:38:34 -0500

    SCHEMA

--- src/middlewares/login.middleware.js
+++ src/middlewares/login.middleware.js
@@ -1,6 +1,6 @@
 import { sessionSchema } from "../schemas/login.schema.js";
 
-export const datosLogin = async (req, res, next) => {
+const datosLogin = async (req, res, next) => {
   try {
     await sessionSchema.validateAsync(req.body);
     next();
@@ -9,3 +9,5 @@ export const datosLogin = async (req, res, next) => {
     res.status(404).json(error);
   }
 };
+
+export default datosLogin;
